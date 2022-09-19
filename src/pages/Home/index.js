ea971663d59c1cfe96b301d9bbe559260e7e605c
Pio Lima <pio.lima@gmail.com>
2022-09-19 14:07:54 -0400

    Created component Movie

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,24 @@
-import { Container } from "./styles";
+import { Container, MovieList, Movie } from "./styles";
 
 function Home() {
     return (
         <Container>
             <h1>Movies</h1>
-            <ul>
-                <li>
+            <MovieList>
+                <Movie>
                     <a href="https://google.com.br"><img src="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_.jpg" width="300" height="400" alt="spider man"/></a>
                     <span>Spider Man</span>
-                </li>
+                </Movie>
                 
-                <li>
+                <Movie>
                 <a href="https://google.com.br"><img src="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_.jpg" width="300" height="400" alt="spider man"/></a>
                     <span>Spider Man</span>
-                </li>
-                <li>
+                </Movie>
+                <Movie>
                 <a href="https://google.com.br"><img src="https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_.jpg" width="300" height="400" alt="spider man"/></a>
                     <span>Spider Man</span>
-                </li>
-            </ul>
+                </Movie>
+            </MovieList>
         </Container>
          
     )
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -6,19 +6,21 @@ export const Container = styled.div`
         margin: 4rem 0;
     }
 
-    ul {
-        list-style: none;
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-        column-gap: 3rem;
-        row-gap: 4rem;
-    }
-    
-    li {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+`
+export const MovieList = styled.ul`
+
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    column-gap: 3rem;
+    row-gap: 4rem;
+
+`
+
+export const Movie = styled.li `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     img {
         width: 200px;
@@ -39,9 +41,5 @@ export const Container = styled.div`
     a:hover {
         transform: scale(1.1);
     }
-
-`
-export const MovieList = styled.ul`
-
 
 `
